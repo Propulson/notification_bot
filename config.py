29c152589cd83902feb1bd8a57f3b040cd106619
propulson <aeroflotmili@gmail.com>
2025-10-07 12:45:56 +1200

    code reworking

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN ='BOT_TOKEN'
+BOT_TOKEN ='bot_token'
 
 if not BOT_TOKEN or ":" not in BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN не найден! Проверьте файл .env")
